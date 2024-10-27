684153538c6c06145ad1ddab1a1111b8a0d5c661
Besmella Kazimy <b.kazimy5@gmail.com>
2024-10-27 23:53:04 +0000

    Oct 28, 2024, 5:23 AM

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <text>Hello world!</text>
+      <Text>Hello world!</Text>
     </View>
   );
 }
