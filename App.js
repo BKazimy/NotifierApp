c5faa28e188f25c73827e07bde92030f961eb603
Besmella Kazimy <b.kazimy5@gmail.com>
2024-10-27 23:43:29 +0000

    Oct 28, 2024, 5:13 AM

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello, world!</Text>
-      <StatusBar style="auto" />
+      Hello world!
     </View>
   );
 }
