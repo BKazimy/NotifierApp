a50d6de38794d2950160716cc1dde4986b0592b6
Besmella Kazimy <b.kazimy5@gmail.com>
2024-10-28 19:09:50 +0000

    Oct 29, 2024, 12:39 AM

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ export default function App() {
       width:'80%', 
       height: 300, 
       justifyContent: 'space-around', 
-      alignItems: 'center', 
+      alignItems: 'stretch', 
       borderWidth:1, 
       borderColor: 'red',
       }}>
