a1aad37126154074405ef1ce22ecf38535317347
Besmella Kazimy <b.kazimy5@gmail.com>
2024-10-28 04:54:24 +0000

    Oct 28, 2024, 10:24 AM

--- App.js
+++ App.js
@@ -4,14 +4,19 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 export default function App() {
   return (
     // <View style={styles.container}>
-    <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
+    // <View style={styles.appContainer}>
+    <View>
+      <View
+      >
+        <Text>1</Text>
+      </View>
+      {/* <View style={styles.inputContainer}>
         <TextInput style={styles.TextInput} placeholder='Your course name:' />
         <Button title='Add Goal' />
       </View>
       <View>
         <Text>List of Goals...</Text>
-      </View>
+      </View> */}
 
       {/* <Text
         style={styles.dummyStyle}
@@ -27,20 +32,20 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  appContainer: {
-    width: "100%",
-    padding: 50,
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  },
-  TextInput: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    width: '80%',
-    marginRight: 5,
-  }
+  // appContainer: {
+  //   width: "100%",
+  //   padding: 50,
+  // },
+  // inputContainer: {
+  //   flexDirection: 'row',
+  //   justifyContent: 'space-between'
+  // },
+  // TextInput: {
+  //   borderWidth: 1,
+  //   borderColor: '#ccc',
+  //   width: '80%',
+  //   marginRight: 5,
+  // }
 
   // container: {
   //   flex: 1,
