908e31c17bdd45313196fea99731edd9ba4c464e
Besmella Kazimy <b.kazimy5@gmail.com>
2024-10-28 18:53:07 +0000

    Oct 29, 2024, 12:23 AM

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     // <View style={styles.container}>
     // <View style={styles.appContainer}>
-    <View style={{ padding: '9%', flexDirection: 'row', width:'80%', height: 300, justifyContent: 'space-between', alignItems: 'center'}}>
+    <View style={{ padding: '9%', flexDirection: 'row', width:'80%', height: 300, justifyContent: 'space-between', alignItems: 'center', borderWidth:1, borderColor: 'red'}}>
     <View
       style={{
         backgroundColor: 'red',
