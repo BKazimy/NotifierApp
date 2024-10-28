94549f047d32f4022bd5a8cb6d92fb8f2db7c0e4
Besmella Kazimy <b.kazimy5@gmail.com>
2024-10-28 21:02:14 +0000

    Oct 29, 2024, 2:32 AM

--- App.js
+++ App.js
@@ -5,89 +5,97 @@ export default function App() {
   return (
     // <View style={styles.container}>
     // <View style={styles.appContainer}>
-    <View style={{ 
-      padding: '9%', 
-      flexDirection: 'row', 
-      width:'80%', 
-      height: 300, 
-      justifyContent: 'space-around', 
-      alignItems: 'stretch', 
-      borderWidth:1, 
-      borderColor: 'red',
-      }}>
+    // <View style={{ 
+    //   padding: '9%', 
+    //   flexDirection: 'row', 
+    //   width:'80%', 
+    //   height: 300, 
+    //   justifyContent: 'space-around', 
+    //   alignItems: 'stretch', 
+    //   borderWidth:1, 
+    //   borderColor: 'red',
+    //   }}>
 
-    <View
-      style={{
-        backgroundColor: 'red',
-        flex: 1,
-        // width: '33.33334%',
-        // height: 100,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text>1</Text>
-    </View>
-      <View
-        style={{
-          backgroundColor: 'green',
-          flex: 1,
-          // width: '33.33334%',
-          // height: 100,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Text>2</Text>
-      </View>
-      <View
-        style={{
-          backgroundColor: 'blue',
-          flex: 1,
-          // width: '33.33334%',
-          // height: 100,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Text>3</Text>
-      </View>
-      {/* <View style={styles.inputContainer}>
-        <TextInput style={styles.TextInput} placeholder='Your course name:' />
-        <Button title='Add Goal' />
-      </View>
-      <View>
-        <Text>List of Goals...</Text>
-      </View> */}
+    // <View
+    //   style={{
+    //     backgroundColor: 'red',
+    //     flex: 1,
+    //     // width: '33.33334%',
+    //     // height: 100,
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    //   }}
+    // >
+    //   <Text>1</Text>
+    // </View>
+    //   <View
+    //     style={{
+    //       backgroundColor: 'green',
+    //       flex: 1,
+    //       // width: '33.33334%',
+    //       // height: 100,
+    //       justifyContent: 'center',
+    //       alignItems: 'center',
+    //     }}
+    //   >
+    //     <Text>2</Text>
+    //   </View>
+    //   <View
+    //     style={{
+    //       backgroundColor: 'blue',
+    //       flex: 1,
+    //       // width: '33.33334%',
+    //       // height: 100,
+    //       justifyContent: 'center',
+    //       alignItems: 'center',
+    //     }}
+    //   >
+    //     <Text>3</Text>
+    //   </View>
+    //   {/* <View style={styles.inputContainer}>
+    //     <TextInput style={styles.TextInput} placeholder='Your course name:' />
+    //     <Button title='Add Goal' />
+    //   </View>
+    //   <View>
+    //     <Text>List of Goals...</Text>
+    //   </View> */}
 
-      {/* <Text
-        style={styles.dummyStyle}
-      >
-        Hello world!
-        </Text>
-        <Button
-          style={styles.dummyStyle}
-          title="Im a button"
-        /> */}
+    //   {/* <Text
+    //     style={styles.dummyStyle}
+    //   >
+    //     Hello world!
+    //     </Text>
+    //     <Button
+    //       style={styles.dummyStyle}
+    //       title="Im a button"
+    //     /> */}
+    // </View>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='Your Text?'/>
+        <Button title='Submit'/>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  // appContainer: {
-  //   width: "100%",
-  //   padding: 50,
-  // },
-  // inputContainer: {
-  //   flexDirection: 'row',
-  //   justifyContent: 'space-between'
-  // },
-  // TextInput: {
-  //   borderWidth: 1,
-  //   borderColor: '#ccc',
-  //   width: '80%',
-  //   marginRight: 5,
-  // }
+  appContainer: {
+    width: "100%",
+    padding: 50,
+    borderWidth:1,
+    borderColor: 'red',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    width: '80%',
+    marginRight: 5,
+  }
 
   // container: {
   //   flex: 1,
