42851fd04e643fbb8cf946fbee498341973e596e
Besmella Kazimy <b.kazimy5@gmail.com>
2024-10-28 21:38:25 +0000

    Oct 29, 2024, 3:08 AM

--- App.js
+++ App.js
@@ -92,21 +92,22 @@ const styles = StyleSheet.create({
     paddingTop: 50,
     paddingHorizontal: 20,
 
+    flex: 1,
+    // flexDirection: 'column',
     // justifyContent: 'space-between',
-    // alignItems: 'center'
+    // alignItems: 'stretch',
 
     // borderWidth:1,
     // borderColor: 'red',
   },
 
   inputContainer: {
-    // flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
     paddingBottom: 15,
 
-    // flex: 1,
+    flex: 1,
 
     // borderBottomWidth: 1,
     // borderBottomColor: 'gainsboro',
