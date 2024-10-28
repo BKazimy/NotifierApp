3b57f5b5805e0c00a1cb0dfa2be78c04ee0cdfcb
Besmella Kazimy <b.kazimy5@gmail.com>
2024-10-28 04:12:32 +0000

    Oct 28, 2024, 9:42 AM

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
@@ -9,6 +9,10 @@ export default function App() {
       >
         Hello world!
         </Text>
+        <Button
+          style={styles.dummyStyle}
+          title="Im a button"
+        />
     </View>
   );
 }
@@ -20,7 +24,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  
+
   dummyStyle: {
     margin: 16,
     padding: 16,
