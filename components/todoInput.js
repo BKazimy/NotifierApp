03f411558c0214c62a7aa5795583af83b5f4ef00
Besmella Kazimy <b.kazimy5@gmail.com>
2024-10-29 20:44:23 +0000

    Oct 30, 2024, 2:14 AM

--- components/todoInput.js
+++ components/todoInput.js
@@ -71,11 +71,11 @@ const styles = StyleSheet.create({
 
   textInput: {
     width: '100%',
-    height: 40,
+    // height: 40,
     marginRight: 5,
     marginBottom: '10%',
     color: 'whitesmoke',
-    padding: 16,
+    padding: 10,
 
     borderWidth: 1,
     borderColor: 'silver',
