2c8f385abdef03bfdf0b50b220bd3403ba59fb83
Besmella Kazimy <b.kazimy5@gmail.com>
2024-10-29 11:35:23 +0000

    Oct 29, 2024, 5:05 PM

--- App.js
+++ App.js
@@ -22,7 +22,6 @@ export default function App() {
         {text: enteredTodoText, id: Math.random().toString()},
       ]);
       console.log(todos);
-      // setEnteredTodoText('');
     };
   };
 
