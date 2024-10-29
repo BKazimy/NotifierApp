787b518478adaa0b999031de42741082572a8498
Besmella Kazimy <b.kazimy5@gmail.com>
2024-10-29 19:21:52 +0000

    Oct 30, 2024, 12:51 AM

--- components/todoInput.js
+++ components/todoInput.js
@@ -64,7 +64,7 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 40,
     marginRight: 5,
-    flex: 1,
+    // flex: 1,
 
     // justifyContent: 'center',
     // alignItems: 'center',
